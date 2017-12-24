9dee7900a2656c46b3aae68337a510384d8289d8
Or Nachshon <or@yellowheadinc.local>
2017-12-24 12:34:16 +0200

    just uploading the file, not ready yet

---
+++ multiple_linear_regression.R
@@ -0,0 +1,25 @@
+# multiple linear regression 
+
+# Simple Linear Regression
+
+# Importing the dataset
+dataset = read.csv('50_Startups.csv')
+
+
+# Encoding categorical data
+dataset$State = factor(dataset$State,
+                         levels = c('New York', 'California', 'Florida'),
+                         labels = c(1, 2, 3))
+
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
