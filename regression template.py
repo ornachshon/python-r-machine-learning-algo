8357f323715beac702784fefeffb308892ff2fdd
Or Nachshon <or@yellowheadinc.local>
2018-01-02 12:58:29 +0200

    regression template

--- regression template.py
+++ regression template.py
@@ -1,4 +1,6 @@
-# polynomial regression 
+
+
+#regression template 
 
 # importing the libraries
 import numpy as np
@@ -22,13 +24,13 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, rando
 '''from sklearn.preprocessing import StandardScaler
 sc_x = StandardScaler()
 x_train = sc_x.fit_transform(x_train)
-x_test = sc_x.transform(x_test)'''
+x_test = sc_x.transform(x_test)
+sc_y = standardScaler()
+y_train = sc_y.fit_transform(y_train)'''
 
 
 # fitting linear regression to the dataset
-from sklearn.linear_model import LinearRegression
-lin_reg = LinearRegression()
-lin_reg.fit(x, y)
+
 
 # fitting polynomial regression to the dataset
 from sklearn.preprocessing import PolynomialFeatures
